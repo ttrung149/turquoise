5fd90703b09d9cda7cd0250738845bbd237527cc
Trung Truong <ttruong91498@gmail.com>
2021-04-13 10:06:30 -0400

    First commit of logger class

---
+++ src/logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#  Turquoise - VHDL linter and compilation toolchain
+#  Copyright (c) 2020-2021: Turquoise team
+#
+#  File name: logger.py
+#
+#  Description: Implementation of a logger class 
+#
+# -----------------------------------------------------------------------------
+
+class Logger:
+    pass
