ca37538104cca766ae52a9db0c0c0ecda8e8ac3d
Athokshay Ashok <50049047+aashok03@users.noreply.github.com>
2021-04-13 22:04:18 -0400

    Add files via upload

---
+++ src/Logger.py
@@ -0,0 +1,29 @@
+from Error import Error, Warning
+from colored import fg, bg, attr
+import sys
+
+class Logger:
+	
+	#Constructor
+	def __init__(self, logs=[], filename="log.txt"):
+		self.logs = logs
+		self.filename = filename
+
+	#Pretty print the logs to the file
+	def print_logs_to_file(self):
+		original_stdout = sys.stdout
+		with open(self.filename, 'w') as f:
+			sys.stdout = f
+			for x in self.logs:
+				print(x.message)
+			sys.stdout = original_stdout
+
+	#Pretty print the logs to terminal
+	def print_logs_to_terminal(self):
+		for x in self.logs:
+			print(x)
+
+
+
+
+
---
+++ src/LoggerTest.py
@@ -0,0 +1,15 @@
+from Logger import Logger
+from Error import Error, Warning
+
+e1 = Error(5, "input.txt")
+e2 = Error(5, "input.txt", "Generic error")
+w1 = Warning(5, "input.txt")
+w2 = Warning(5, "input.txt", "Generic warning")
+
+logs = [e1, e2, w1, w2]
+logger1 = Logger(logs)
+logger1.print_logs_to_terminal()
+logger1.print_logs_to_file()
+
+logger2 = Logger(logs, "log2.txt")
+logger2.print_logs_to_file()
