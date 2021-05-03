6dd8c9907eb30297d5ede757c648011320df248c
Trung Truong <ttruong91498@gmail.com>
2021-05-03 15:52:57 -0400

    Revamp linter, add entity component typecheck

--- src/Linter.py
+++ src/Linter.py
@@ -1,20 +1,90 @@
-from Logger import Logger
-from Error import Error, Warning
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#  Turquoise - VHDL linter and compilation toolchain
+#  Copyright (c) 2020-2021: Turquoise team
+#
+#  File name: Linter.py
+#
+#  Description: Implementation of Linter class that performs linting on
+#  provided files. Refer to README.md for supported features.
+#
+# -----------------------------------------------------------------------------
+from pyVHDLParser.Token import StartOfDocumentToken, EndOfDocumentToken, \
+                               SpaceToken, LinebreakToken, CommentToken, \
+                               IndentationToken
+from pyVHDLParser.Base import ParserException
+from itertools import chain
+from .Messages import Error, Warning
+from .Tokenize import Tokenize
+from .Entity import parse_entity_component
+from .TypeCheck import tc_entity_component
 
 class Linter:
 
-	def __init__(self, filename):
-		self.filename = filename
-		self.logs = []
+    def __init__(self, _filenames, _logger):
+        self._filenames = _filenames
+        self._logger = _logger
 
+    def print_status(self):
+        # @TODO: add more stats
+        self._logger.print_logs_to_terminal()
+        self._logger.print_logs_to_file()
 
-	def get_logger(self):
-		return Logger(logs=self.logs)
+    def lint(self):
+        """ Perform linting on provided files """
 
+        # Global states
+        entity_dict = {}
+        component_list = []
 
-	def lint(self):
-		e1 = Error(5, "input.txt")
-		w1 = Warning(6, "input.txt", "Generic warning")
+        # Lint through a list of files
+        for f in self._filenames:
+            tokenize = Tokenize(f)
+            token_iter = tokenize.get_token_iter()
 
-		self.logs.append(e1)
-		self.logs.append(w1)
+            try:
+                for token in token_iter:
+                    # Skip comment and linebreaks
+                    if isinstance(token, (StartOfDocumentToken, EndOfDocumentToken,
+                                  LinebreakToken, SpaceToken,
+                                  IndentationToken, CommentToken)):
+                        continue
+
+                    # Parse entity
+                    if token.Value.lower() == 'entity':
+                        token_iter = chain([token], token_iter)
+                        entity = parse_entity_component(token_iter, self._logger, f)
+
+                        if entity is not None:
+                            if entity[0] in entity_dict:
+                                duplicated_entity_filename = entity_dict[entity[0]][0]
+                                warn = Warning(token.Start, f,
+                                               'Duplicated entity declaration found. ' +
+                                               'Entity "' + entity[0] +
+                                               '" was previously declared in "' +
+                                               duplicated_entity_filename + '"')
+                                self._logger.add_log(warn)
+                            else:
+                                entity_dict[entity[0]] = (f, entity)
+
+                    # Parse component
+                    elif token.Value.lower() == 'component':
+                        token_iter = chain([token], token_iter)
+                        component = parse_entity_component(token_iter, self._logger, f)
+
+                        if component is not None:
+                            component_list.append((component[0], f, component))
+
+                    else:
+                        print('TOKEN IS {}'.format(token))
+                        continue
+
+            except ParserException as ex:
+                err = Error(token.Start, f, str(ex))
+                self._logger.add_log(err)
+
+            except NotImplementedError as ex:
+                err = Error(token.Start, f, str(ex))
+                self._logger.add_log(err)
+
+        tc_entity_component(entity_dict, component_list, self._logger)
---
+++ src/TypeCheck.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#  Turquoise - VHDL linter and compilation toolchain
+#  Copyright (c) 2020-2021: Turquoise team
+#
+#  File name: TypeCheck.py
+#
+#  Description: Implementation of typechecking functions
+#
+# -----------------------------------------------------------------------------
+from .Messages import Error, Info
+
+def tc_entity_component(_entity_dict, _component_list, _logger):
+    """
+    @brief Helper function. Typecheck component against entity declaration
+    @param _entity_dict
+    @param _component_list
+    @param _logger Logger instance
+    @return None
+    """
+
+    for component in _component_list:
+        component_name, component_filename, parsed_component = component
+
+        if component_name not in _entity_dict:
+            err = Error('', component_filename, 'Component "' + component_name +
+                        '" has not been declared.')
+            _logger.add_log(err)
+
+        else:
+            entity_file_name = _entity_dict[component_name][0]
+            entity_name, entity_generics, entity_ports = _entity_dict[component_name][1]
+            comp_name, comp_generics, comp_ports = parsed_component
+
+            if len(comp_generics) != len(entity_generics) or \
+               len(comp_ports) != len(entity_ports) :
+                err = Error('', component_filename, 'Component "' + component_name +
+                            '" does not match declared entity "' + entity_name + '" @ "' +
+                            entity_file_name + '"')
+                _logger.add_log(err)
+
+                info = Info('hint - check each signal in the component and its matching entity')
+                _logger.add_log(info)
+
+            # Compare generic signals against entity declaration
+            for sig in comp_generics:
+                if sig not in entity_generics:
+                    err = Error('', component_filename, '\nGeneric signal "' + sig +
+                                '" in component "' + comp_name + '"' +
+                                '" is not declared in entity "' + entity_name + '" @ "' +
+                                entity_file_name + '"')
+                    _logger.add_log(err)
+                
+                elif comp_generics[sig] != entity_generics[sig]:
+                    err = Error('', component_filename, '\nGeneric signal "' + sig +
+                                '" in component "' + entity_name + '" has type "' + 
+                                str(comp_generics[sig]) + ', but is declared to have type "' +
+                                str(entity_generics[sig]) + '" in "' + entity_file_name + '"')
+                    _logger.add_log(err)
+
+            # Compare ports signals against entity declaration
+            for sig in comp_ports:
+                if sig not in entity_ports:
+                    err = Error('', component_filename, '\nPort signal "' + sig +
+                                '" in component "' + comp_name + '"' +
+                                '" is not declared in entity "' + entity_name + '" @ "' +
+                                entity_file_name + '"')
+                    _logger.add_log(err)
+                
+                elif comp_ports[sig] != entity_ports[sig]:
+                    err = Error('', component_filename, '\nPort signal "' + sig +
+                                '" in component "' + entity_name + '" has type "' + 
+                                str(comp_ports[sig][1]) + '" (' + comp_ports[sig][0] + ')' +
+                                ', but is declared to have type "' +
+                                str(entity_ports[sig][1]) + '" (' + entity_ports[sig][0] + ')'
+                                + ' in "' + entity_file_name + '"')
+                    _logger.add_log(err)
