40170d75a10372f1f488700c7e09f9c3036cbf70
Trung Truong <ttruong91498@gmail.com>
2021-05-03 09:00:59 -0400

    Add basic main and __init__.py for packaging

---
+++ __main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#  Turquoise - VHDL linter and compilation toolchain
+#  Copyright (c) 2020-2021: Turquoise team
+#
+#  File name: __main__.py
+#
+#  Description: Main driver for the turquoise VDHL linter + compilation
+#  toolchain. Refer to README.md for specific usage
+#
+# -----------------------------------------------------------------------------
+from src.ArgParser import ArgParser
+
+if __name__ == '__main__':
+    ArgParser()
